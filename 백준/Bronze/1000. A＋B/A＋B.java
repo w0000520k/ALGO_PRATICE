197d55d613f07b37f81495cc4f6df4f81b6e683d
w0000520k <117965153+w0000520k@users.noreply.github.com>
2024-04-08 19:34:31 +0900

    [Bronze V] Title: A+B, Time: 124 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int A, B;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        A = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(st.nextToken());
+        
+        System.out.print(A+B);
+    }
+}
