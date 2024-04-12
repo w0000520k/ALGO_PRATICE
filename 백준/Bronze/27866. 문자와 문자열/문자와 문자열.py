9027d6647c1f80dc8d04db9f652ec2ba6e4a4772
w0000520k <117965153+w0000520k@users.noreply.github.com>
2024-04-12 18:55:33 +0900

    [Bronze V] Title: 문자와 문자열, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/27866. 문자와 문자열/문자와 문자열.py
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.py
@@ -1,16 +1,3 @@
-import sys
-
-text = input()
-if len(text) > 1000:
-    sys.exit()
-
-if not text.isalpha() :
-    sys.exit()
-
-number = input()
-number_int = int(number)
-
-if number_int > len(text) :
-    sys.exit()
-else:
-    print(text[number_int - 1])
+S = input()
+i = int(input())
+print(S[i-1])
